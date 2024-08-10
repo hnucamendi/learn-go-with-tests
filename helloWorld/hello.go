82ee5c27f096b7b9774e6687ac4f7f48945c88e3
Harold Arriola Nucamendi <55587068+hnucamendi@users.noreply.github.com>
2024-08-10 09:12:21 -0400

    refactoring some more

--- helloWorld/hello.go
+++ helloWorld/hello.go
@@ -2,15 +2,84 @@ package main
 
 import "fmt"
 
-const englishHelloWorldPrefix string = "Hello "
+type Message struct {
+	Language Lang
+	Prefix   MessagePrefix
+}
 
-func Hello(s string) string {
-	if s == "" {
-		return englishHelloWorldPrefix + "World!"
+type Lang string
+type MessagePrefix string
+
+const (
+	SpanishLang    Lang          = "SPANISH"
+	EnglishLang    Lang          = "ENGLISH"
+	FrenchLang     Lang          = "FRENCH"
+	JapaneseLang   Lang          = "JAPANESE"
+	OtherLang      Lang          = "OTHER"
+	englishPrefix  MessagePrefix = "Hello"
+	spanishPrefix  MessagePrefix = "Hola"
+	japanesePrefix MessagePrefix = "こんにちは"
+	frenchPrefix   MessagePrefix = "Bonjour"
+)
+
+func buildString(p MessagePrefix, s string) string {
+	return fmt.Sprintf("%s %s!", p, s)
+}
+
+func English() *Message {
+	return &Message{
+		Language: EnglishLang,
+		Prefix:   englishPrefix,
 	}
-	return englishHelloWorldPrefix + s + "!"
+}
+func Spanish() *Message {
+	return &Message{
+		Language: SpanishLang,
+		Prefix:   spanishPrefix,
+	}
+}
+func French() *Message {
+	return &Message{
+		Language: FrenchLang,
+		Prefix:   frenchPrefix,
+	}
+}
+func Japanese() *Message {
+	return &Message{
+		Language: JapaneseLang,
+		Prefix:   japanesePrefix,
+	}
+}
+
+func Config(l Lang) *Message {
+	switch l {
+	case SpanishLang:
+		return Spanish()
+	case FrenchLang:
+		return French()
+	case JapaneseLang:
+		return Japanese()
+	default:
+		return English()
+	}
+}
+
+func (m *Message) Hello(s string) string {
+	if s == "" {
+		s = "World"
+	}
+
+	return buildString(m.Prefix, s)
 }
 
 func main() {
-	fmt.Println(Hello("David"))
+	english := Config(EnglishLang)
+	spanish := Config(SpanishLang)
+	french := Config(FrenchLang)
+	japanese := Config(JapaneseLang)
+
+	fmt.Println(english.Hello("David"))  // Hello David!
+	fmt.Println(spanish.Hello("David"))  // Hola David!
+	fmt.Println(french.Hello("David"))   // Bonjour David!
+	fmt.Println(japanese.Hello("David")) // こんにちは David!
 }
--- helloWorld/hello_test.go
+++ helloWorld/hello_test.go
@@ -3,15 +3,49 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	t.Run("test1", func(t *testing.T) {
-		got := Hello("COATL")
+	t.Run("in english", func(t *testing.T) {
+		msg := Config(EnglishLang)
+		got := msg.Hello("COATL")
+		want := "Hello COATL!"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in spanish", func(t *testing.T) {
+		msg := Config(SpanishLang)
+		got := msg.Hello("COATL")
+		want := "Hola COATL!"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in french", func(t *testing.T) {
+		msg := Config(FrenchLang)
+		got := msg.Hello("COATL")
+		want := "Bonjour COATL!"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in japanese", func(t *testing.T) {
+		msg := Config(JapaneseLang)
+		got := msg.Hello("COATL")
+		want := "こんにちは COATL!"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in other language", func(t *testing.T) {
+		msg := Config(EnglishLang)
+		got := msg.Hello("COATL")
 		want := "Hello COATL!"
 
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("no parameter passed in", func(t *testing.T) {
-		got := Hello("")
+		msg := Config(EnglishLang)
+		got := msg.Hello("")
 		want := "Hello World!"
 
 		assertCorrectMessage(t, got, want)
